2c669d10611cc6c0ec3171e7371b6f655ca74414
unknown <ghaliarose89@gmail.com>
2021-09-25 12:42:26 -0500

    touchups

--- assets/javascript/js.js
+++ assets/javascript/js.js
@@ -1,17 +1,16 @@
-
+// arr to save tasks
 var task = [];
-
+// displaying the current time
 var timeEl = document.querySelector ("#currentDay");
 timeEl.textContent= moment().format("MM/D/ YYYY  H:MM a");
 
+// function to get the tasks and hours frm local-storage
 var showPlan = function (){
     var displayPlan= JSON.parse(localStorage.getItem ("plan")) || []
-    console.log(displayPlan);
+
     for ( var i = 0; i <displayPlan.length; i ++) {
-        console.log (displayPlan[i]);
-        var hour = displayPlan[i].hours;
-        $("#txt-" + hour).val(displayPlan[i].userTask);
-        console.log (displayPlan[i].userTask); 
+        var hour = displayPlan[i].hours;// giting hour to display it and git the corrisponding task for the same hour
+        $("#txt-" + hour).val(displayPlan[i].userTask); 
     }
 };
 
@@ -19,8 +18,9 @@ var showPlan = function (){
 var checkColor = function () {
 var currentTime = parseInt(moment().format("H"));
  $(".time-block").each(function(){
-    var blockTime = parseInt($(this).attr('id'));
+    var blockTime = parseInt($(this).attr('id'));// pars to get the value as number to commpare it with current time
     $(this).removeClass("past present future");
+    
     if (blockTime < currentTime){
         $(this).addClass("past");
     }
@@ -37,13 +37,10 @@ showPlan();
 
 $(document).on('click', '.saveBtn', function(event) {
         event.preventDefault();
-        var itemId = $(this).parent("div.row").attr("id");
-        console.log (itemId);
+        var itemId = $(this).parent("div.row").attr("id");//giting the div id to get the corrisponding text area 
         var textinput = $("#txt-" + itemId).val();
-        console.log(textinput);
        task = JSON.parse ( localStorage.getItem("plan")) || [];
         task.push ( {hours:itemId ,  userTask : textinput});
-        console.log (task);
         localStorage.setItem ("plan",JSON.stringify(task));
      } );
             
